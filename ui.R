d24625a107fa373c955187320af3df2f857b8e0c
Erich Peterson <contact@erichpeterson.com>
2019-05-07 13:35:12 -0500

    Refactored code in ui.R to conform more to standard R styling.

--- ui.R
+++ ui.R
@@ -10,21 +10,20 @@ library(RSQLite)
 dbhandle <- dbConnect(RSQLite::SQLite(), "latest.db")
 
 #get the sample ids in the database
-res = dbGetQuery(dbhandle, 'SELECT sample_id from GENOM_SAMPLE')
+res <- dbGetQuery(dbhandle, "SELECT sample_id from GENOM_SAMPLE")
 options(shiny.sanitize.errors = TRUE)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
   # Application title
   titlePanel("TarPan Viewer"),
-  img(src='logo.jpg', align = "right"),
-  tags$head(tags$link(rel="shortcut icon", href="favicon.ico")),
+  img(src = "logo.jpg", align = "right"),
+  tags$head(tags$link(rel = "shortcut icon", href = "favicon.ico")),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      width=2,
+      width = 2,
       h4("Data Filtering Options"),
       checkboxInput("type", "Show all chromosomes", value = 1, width = NULL),
       conditionalPanel(
@@ -37,59 +36,70 @@ shinyUI(fluidPage(
                   step = 1
                   )
       ),
-      checkboxInput("chromnorm", "Use specific chromosomes for normalization", value = F, width=NULL),
+      checkboxInput("chromnorm", "Use specific chromosomes for normalization", 
+                    value = FALSE, width = NULL),
       conditionalPanel(
         condition = "input.chromnorm == 1",
         fluidRow(
           column(4,
-            checkboxInput("chrom1", "1", value = F, width= NULL),
-            checkboxInput("chrom2", "2", value = F, width= NULL),
-            checkboxInput("chrom3", "3", value = F, width= NULL),
-            checkboxInput("chrom4", "4", value = F, width= NULL),
-            checkboxInput("chrom5", "5", value = F, width= NULL),
-            checkboxInput("chrom6", "6", value = F, width= NULL),
-            checkboxInput("chrom7", "7", value = F, width= NULL),
-            checkboxInput("chrom8", "8", value = F, width= NULL)
+            checkboxInput("chrom1", "1", value = FALSE, width = NULL),
+            checkboxInput("chrom2", "2", value = FALSE, width = NULL),
+            checkboxInput("chrom3", "3", value = FALSE, width = NULL),
+            checkboxInput("chrom4", "4", value = FALSE, width = NULL),
+            checkboxInput("chrom5", "5", value = FALSE, width = NULL),
+            checkboxInput("chrom6", "6", value = FALSE, width = NULL),
+            checkboxInput("chrom7", "7", value = FALSE, width = NULL),
+            checkboxInput("chrom8", "8", value = FALSE, width = NULL)
           ),
           column(4,
-            checkboxInput("chrom9", "9", value = F, width= NULL),
-            checkboxInput("chrom10", "10", value = F, width= NULL),
-            checkboxInput("chrom11", "11", value = F, width= NULL),
-            checkboxInput("chrom12", "12", value = F, width= NULL),
-            checkboxInput("chrom13", "13", value = F, width= NULL),
-            checkboxInput("chrom14", "14", value = F, width= NULL),
-            checkboxInput("chrom15", "15", value = F, width= NULL),
-            checkboxInput("chrom16", "16", value = F, width= NULL)
+            checkboxInput("chrom9", "9", value = FALSE, width = NULL),
+            checkboxInput("chrom10", "10", value = FALSE, width = NULL),
+            checkboxInput("chrom11", "11", value = FALSE, width = NULL),
+            checkboxInput("chrom12", "12", value = FALSE, width = NULL),
+            checkboxInput("chrom13", "13", value = FALSE, width = NULL),
+            checkboxInput("chrom14", "14", value = FALSE, width = NULL),
+            checkboxInput("chrom15", "15", value = FALSE, width = NULL),
+            checkboxInput("chrom16", "16", value = FALSE, width = NULL)
           ),
           column(4,
-            checkboxInput("chrom17", "17", value = F, width= NULL),
-            checkboxInput("chrom18", "18", value = F, width= NULL),
-            checkboxInput("chrom19", "19", value = F, width= NULL),
-            checkboxInput("chrom20", "20", value = F, width= NULL),
-            checkboxInput("chrom21", "21", value = F, width= NULL),
-            checkboxInput("chrom22", "22", value = F, width= NULL)
+            checkboxInput("chrom17", "17", value = FALSE, width = NULL),
+            checkboxInput("chrom18", "18", value = FALSE, width = NULL),
+            checkboxInput("chrom19", "19", value = FALSE, width = NULL),
+            checkboxInput("chrom20", "20", value = FALSE, width = NULL),
+            checkboxInput("chrom21", "21", value = FALSE, width = NULL),
+            checkboxInput("chrom22", "22", value = FALSE, width = NULL)
           )
         )
       ),
-      checkboxInput("show_blacklist", "Show blacklist genes", value=F, width=NULL),
+      checkboxInput("show_blacklist", "Show blacklist genes", 
+                    value = FALSE, width=NULL),
       conditionalPanel(
         "$('li.active a').first().html() == 'Mutations'",
         h4("Mutation Options"),
-        checkboxInput("all_mutation_information", "Show all mutation information", value = F, width = NULL),
-        checkboxInput("show_failed_mutations", "Show mutations that failed filter", value = F, width = NULL),
-        checkboxInput("hide_blank_mutations", "Hide blank mutations", value = F, width = NULL)
+        checkboxInput("all_mutation_information", 
+                      "Show all mutation information", 
+                      value = FALSE, width = NULL),
+        checkboxInput("show_failed_mutations", 
+                      "Show mutations that failed filter", 
+                      value = FALSE, width = NULL),
+        checkboxInput("hide_blank_mutations", 
+                      "Hide blank mutations", 
+                      value = FALSE, width = NULL)
       ),
       conditionalPanel(
         "$('li.active a').first().html() == 'Structural Variants'",
         h4("Structural Variant Options"),
-        checkboxInput("show_failed_structvars", "Show SVs that failed filter", value = F, width = NULL)
+        checkboxInput("show_failed_structvars", "Show SVs that failed filter", 
+                      value = FALSE, width = NULL)
       ),
       conditionalPanel(
         "$('li.active a').first().html() == 'RCircos'",
         h4("Structural Variant Options"),
-        checkboxInput("show_inter_svs", "Show Inter-chromosomal SVs", value = F, width = NULL),
+        checkboxInput("show_inter_svs", "Show Inter-chromosomal SVs", 
+                      value = FALSE, width = NULL),
         h4("Mutation Options"),
-        checkboxInput("hide_circos_muts", "Hide Mutations", value = F, width = NULL)
+        checkboxInput("hide_circos_muts", "Hide Mutations", 
+                      value = FALSE, width = NULL)
 
       ),
       conditionalPanel(
@@ -97,29 +107,34 @@ shinyUI(fluidPage(
         h4("Plotting Options"),
         conditionalPanel(
           condition = "input.type != 1",
-          checkboxInput("continuous", "Continuous output", value = FALSE, width=NULL)
+          checkboxInput("continuous", "Continuous output", 
+                        value = FALSE, width=NULL)
         ),
         conditionalPanel(
           condition = "input.type != 1 & input.continuous == 1",
           checkboxInput("showlast", "Indicate first gene position")
         ),
-        checkboxInput("pandq", "Show p and q boundaries", value = FALSE, width = NULL),
-        checkboxInput("nolim", "Change max limit", value=F, width=NULL),
+        checkboxInput("pandq", "Show p and q boundaries", 
+                      value = FALSE, width = NULL),
+        checkboxInput("nolim", "Change max limit", 
+                      value = FALSE, width = NULL),
         conditionalPanel(
           condition = "input.nolim == 1",
           textInput("maxlim", label = "Maximum mean ratio", value = "0")
         ),
         
-        checkboxInput("normline", "Show normal line", value=F,width=NULL),
+        checkboxInput("normline", "Show normal line", 
+                      value = FALSE, width = NULL),
         conditionalPanel(
           condition = "input.type != 1",
-          checkboxInput("showsnps", "Show SNPs for these regions", value=F, width=NULL)
+          checkboxInput("showsnps", "Show SNPs for these regions", 
+                        value = FALSE, width = NULL)
         )
       ),
       conditionalPanel(
         condition = "0 == 1",
-        textInput("currentchrom", label="", value = -1),
-        textInput("currentregion", label="", value = "ALL")
+        textInput("currentchrom", label = "", value = -1),
+        textInput("currentregion", label = "", value = "ALL")
       ),
       downloadButton('downloadData', 'Download'),
       downloadButton('downloadCopyNumberPlot', 'Download CN Plot'),
@@ -131,14 +146,16 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      width=10,
-      selectInput("sample", width='100%',
-                           "Sample:",selected = as.character(res$sample_id[1]),
+      width = 10,
+      selectInput("sample", width = '100%',
+                           "Sample:", selected = as.character(res$sample_id[1]),
                            c(unique(as.character(res$sample_id)))),
       tabsetPanel(type = "tabs", 
-                  tabPanel("Copy Number Plot", plotOutput("plot"), plotOutput("plot2")),
+                  tabPanel("Copy Number Plot", plotOutput("plot"), 
+                           plotOutput("plot2")),
                   tabPanel("Mutations", DT::dataTableOutput("mutations_table")),
-                  tabPanel("Structural Variants", DT::dataTableOutput("manta_table")),
+                  tabPanel("Structural Variants", 
+                           DT::dataTableOutput("manta_table")),
                   tabPanel("QC Metrics", DT::dataTableOutput("metrics_table")),
                   tabPanel("AutoCN", DT::dataTableOutput("AutoCN")),
                   tabPanel("AutoCNGroups", DT::dataTableOutput("AutoCNGroups")),
